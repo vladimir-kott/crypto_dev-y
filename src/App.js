9b64bb28904a89656ec6a667f4eeed796488c021
Vladimir Kott <vladimirkott19@gmail.com>
2022-06-17 18:50:41 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Main from './app/layouts/main';
 import NavPanel from './app/components/navPanel';
-import Ticker from './app/components/ticker';
+import TikerComponent from './app/components/ticker';
 import MobileNav from './app/components/mobileNav';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <div className='flex flex-col items-center h-screen bg-gradient-to-tr from-[#EAECF4] to-[#E6E7FD] border-2 border-black'>
         <div className="box-border flex sam:flex-col sus:flex-col scm:flex-col sxm:flex-col sm:flex-row justify-center items-center gap-x-[5px] w-full max-w-[1920px] border-2 border-black h-full">
         <div className="border-2 border-rose-400 text-center leading-loose text-sm sm:p-[20px] sus:p-[2px] sam:p-[7px] scm:p-[7px] sxm:p-[7px] br-black w-full h-full">
-          <Ticker/>
+         <TikerComponent/>
           <Main/>
         </div>
         <div className="sus:hidden sam:hidden scm:hidden sxm:hidden sm:block border-2 border-black text-center leading-loose text-sm p-[20px] rounded-[10px] br-black w-34 h-full">
--- src/app/components/ticker.jsx
+++ src/app/components/ticker.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-const Ticker = () => {
-    return ( 
-        <div className="flex w-full shrink-0 items-center justify-center text-center shadow-md rounded-lg 
+const TickerComponent = () => {
+    const [string, setString] = useState("");
+
+    useEffect(() => {
+        setString('Aliquam consequat varius consequat.')
+    }, []);
+
+    console.log(string)
+
+    return (
+    <div className="flex w-full shrink-0 items-center justify-center text-center shadow-md rounded-lg 
         sm:mb-5 sus:mb-1 sam:mb-2 scm:mb-2 'sxm:mb-3 p-2 bg-[#f9f8ff]
         text-zinc-500">
-            <p>Tiker</p>
+       <div class="overflow-hidden w-full">{/*hwrap*/}
+         <div class="hmove">{/*hmove*/}
+            <div class="hitem">{string}</div>
+            <div class="hitem">Aliquam consequat varius consequat.</div>
+            <div class="hitem">{string}</div>
+            <div class="hitem">Pellentesque auctor molestie orci.</div>
+         </div>
         </div>
+    </div>
     );
 }
  
-export default Ticker;
+export default TickerComponent;
